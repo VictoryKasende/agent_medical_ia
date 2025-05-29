bf40126f70b4b993757690e6f11b3c34b4c50daf
victory <victorykasende@gmail.com>
2025-05-29 14:35:16 +0100

    Update database settings to support development mode and environment variable configuration

--- agent_medical_ia/settings.py
+++ agent_medical_ia/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 import sys
+import dj_database_url
 
 from langchain_community.vectorstores import Redis
 
@@ -91,12 +92,21 @@ WSGI_APPLICATION = 'agent_medical_ia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
